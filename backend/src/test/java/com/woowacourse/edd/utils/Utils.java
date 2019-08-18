0ccb39757acaad0f21cd5e3ed083193eb43bf750
heebong <heebg2@naver.com>
2019-08-18 13:44:17 +0900

    [test] add utils class

--- backend/src/test/java/com/woowacourse/edd/utils/Utils.java
+++ backend/src/test/java/com/woowacourse/edd/utils/Utils.java
@@ -1,4 +1,12 @@
 package com.woowacourse.edd.utils;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Utils {
+    public static String getFormedDate() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHH");
+        return now.format(formatter);
+    }
 }
